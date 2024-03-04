99894325e90f5fe7bcc496f95fcabe4794c9e234
salmandanish03 <162048472+salmandanish03@users.noreply.github.com>
2024-03-04 22:01:24 +0500

    Add files via upload

---
+++ 13_Your_own_array.ts
@@ -0,0 +1,6 @@
+let transportation :string[] = ['Motorcycle','Car','Train','AirPlan'] ;
+
+for(let i=0; i<transportation.length; i++){
+    console.log('I would like to own a ' + transportation[i]);
+
+}
