c81f08f7bda4207fba2896e2f3f94186378b9810
salmandanish03 <162048472+salmandanish03@users.noreply.github.com>
2024-03-04 22:01:57 +0500

    Add files via upload

---
+++ 14_Guest_List.ts
@@ -0,0 +1,6 @@
+let Guest_List :string[] = ['Fawwad Ali','Asad Hyder','Wajahat Hussain Malik'];
+
+for(let i=0; i<Guest_List.length; i++){
+    console.log('Dr. Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
+
+}
