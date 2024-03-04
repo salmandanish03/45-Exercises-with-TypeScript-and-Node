aaf3c47bf4dc166f2c8f194b49669467f96640f5
salmandanish03 <162048472+salmandanish03@users.noreply.github.com>
2024-03-04 22:10:18 +0500

    Add files via upload

---
+++ 20_Empty_Array.ts
@@ -0,0 +1,12 @@
+
+// Think of something you could store in a array. For example, mountains, rivers, countries, cities, languages, or anything else you’d like.
+let items :string[] = ['Pakistan', 'Karachi', 'Urdu'];
+console.log(items);
+
+// Method 2 Write code that creates a list 
+let items2 :string[] = [];
+items2.push('Austrailia');
+items2.push('Dubai');
+items2.push('Rome');
+items2.push('Bali');
+console.log(items2);
