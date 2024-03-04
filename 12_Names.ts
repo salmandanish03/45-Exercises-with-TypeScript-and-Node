1640908e5b2490d7d1bd5550ef257fc111f1696f
salmandanish03 <162048472+salmandanish03@users.noreply.github.com>
2024-03-04 22:00:47 +0500

    Add files via upload

---
+++ 12_Names.ts
@@ -0,0 +1,7 @@
+let names :string[] = ['Salman','Fawwad','Asad','Wajahat','Hamza'];
+
+const message :string = 'Welcome to Typescript Exercises Class: '
+
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]) ;
+}
