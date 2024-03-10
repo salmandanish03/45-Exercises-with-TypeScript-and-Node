5b56ac876366b1f7538c101592cc8d1cbabc5a63
salmandanish03 <162048472+salmandanish03@users.noreply.github.com>
2024-03-10 21:09:12 +0500

    Add files via upload

---
+++ 22_Intentional_Error.ts
@@ -0,0 +1,12 @@
+// If you haven’t received an array index error in one of your programs yet, try to make one happen.
+
+import { Console } from "console";
+
+// Creating Array
+let newArray :string[] = ['Fawwad Ali', 'Asad Hyder', 'Wajahat Hussain'];
+
+// Accessing array by index
+console.log(newArray[5]);
+
+// Correct the error
+console.log(newArray);
