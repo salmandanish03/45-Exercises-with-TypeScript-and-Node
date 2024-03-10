6fd1fed9db5fa6c06a2fb0116cf218796eefc371
salmandanish03 <162048472+salmandanish03@users.noreply.github.com>
2024-03-10 21:20:40 +0500

    Add files via upload

---
+++ 31_No_Users.ts
@@ -0,0 +1,15 @@
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// If the list is empty, print the message We need to find some users!
+
+// Remove all of the usernames from your array, and make sure the correct message is printed.
+
+let userNames: string[] = ['admin', 'Tahir', 'Khan Sahab', 'Abdul Sattar', 'Zulqarnain',];
+if(userNames.length === 0){
+    console.log('We need to find some users');
+}
+else{
+    userNames = [];
+    console.log('All the userNames have been removed.' + userNames.length);
+    
+}
+
