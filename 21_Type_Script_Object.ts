a46fc6f232b23aefb0eb7bca7f4d7a2a9a2358a8
salmandanish03 <162048472+salmandanish03@users.noreply.github.com>
2024-03-10 21:08:31 +0500

    Add files via upload

---
+++ 21_Type_Script_Object.ts
@@ -0,0 +1,34 @@
+// Write a program that creates Objects containing these items.
+
+// data of person object
+interface person {
+    age : number ,
+    name : string,
+    nationality : string,
+    student : Boolean,
+}
+// person object
+let person :person = {
+    age : 38 ,
+    name : 'Salman',
+    nationality : 'Pakistani',
+    student : true,
+}
+// print person
+console.log(person);
+
+// data type of car object 
+interface car{
+    maker : string,
+    color : string,
+    Manual : boolean,
+}
+// Car object
+let car = {
+    maker : 'toyota',
+    color : 'reD',
+    Manual : true,
+    }
+
+    console.log(car);
+    
