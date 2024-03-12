2bd4d2ec45640a425461a1a2209820122a133c0a
salmandanish03 <162048472+salmandanish03@users.noreply.github.com>
2024-03-12 22:16:01 +0500

    Add files via upload

---
+++ homeWork1.ts
@@ -0,0 +1,48 @@
+
+// Exercise Addition
+let ball1: number = 3;
+let ball2: number = 4;
+let resultBalls: number = (ball1+ball2);
+console.log("Result is", resultBalls);
+    
+// Exercise Subtraction
+let cart: number = 6;
+let num2: number = 4;
+let result: number = (cart-num2);
+console.log("Result is", result);
+
+// Exercise Multiplication
+let apples : number = 6;
+let bags: number = 4;
+let result1: number = (apples*bags);
+console.log("Result is", result1);
+
+// Exercise dividation
+let totalApples : number = 30;
+let shoppingBags: number = 4;
+let result2: number = (totalApples/shoppingBags);
+console.log("Result is", result2);
+
+// Exercise Squard
+let layer: number = 4;
+let oragne: number = 4;
+let power: number = 4**2;
+console.log("Result is", power);
+
+// Exercise Modulus / reminder
+let banana : number = 7;
+let shoppingBags1 : number =3;
+let reminder : number = banana % shoppingBags1;
+console.log("Result is", reminder);
+
+
+// Unary Operators
+let a : number = 5;
+let b : number = 2;
+let c : number = ++a +a++ + --b +b-- +a + b++ +b;
+console.log("Result is", c);
+
+// Combining Operators
+let result3 = 3+ 4*5;
+result3++
+console.log("Result is", result3);
